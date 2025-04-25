fb51988bf2ff9f8310bc02efef4c2f1ed672fe9e
Gabriel Huertas <gabriel.huertasdl@gmail.com>
2025-04-25 18:58:32 +0200

    Añadido style.css, script.js y cambios en index.html

---
+++ mi-web/script.js
@@ -0,0 +1,5 @@
+// Opcional: podrías agregar funcionalidades interactivas aquí
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página cargada con estilo Lockheed 🚀');
+});
+Ñ
